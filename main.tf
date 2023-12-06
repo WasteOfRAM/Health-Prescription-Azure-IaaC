fca9a2cd0aa5a45a5124a9748b729d6775c3c9e4
Ivan Kukutsov <ivankukucov@gmail.com>
2023-12-06 18:03:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_service_plan" "linux_service_plan" {
 }
 
 resource "azurerm_linux_web_app" "web_app" {
-  name                = "Health_Prescription"
+  name                = var.web_app_name
   resource_group_name = azurerm_resource_group.HPA_RG.name
   location            = azurerm_resource_group.HPA_RG.location
   service_plan_id     = azurerm_service_plan.linux_service_plan.id
@@ -39,7 +39,7 @@ resource "azurerm_linux_web_app" "web_app" {
   
   site_config {
     application_stack {
-      dotnet_version = 8.0
+      dotnet_version = "8.0"
     }
   }
 }
