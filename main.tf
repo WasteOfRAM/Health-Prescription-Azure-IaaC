ca670c9f1aa209f79d21161651ac30110ef5a543
Ivan Kukutsov <ivankukucov@gmail.com>
2023-12-06 14:41:56 +0200

    backend again

--- main.tf
+++ main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "3.83.0"
     }
   }
-
-    backend "azurerm" {
-    storage_account_name = "terraformfilesstorage"
-    container_name       = "statefiles"
-    key                  = "terraform.tfstate"
-    access_key           = "${ACCESS_KEY}"
-  }
 }
 
 provider "azurerm" {
