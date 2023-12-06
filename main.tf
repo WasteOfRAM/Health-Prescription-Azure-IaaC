4e10789fb5d866bf0ea66aa836d39b40e8ab1aa7
Ivan Kukutsov <ivankukucov@gmail.com>
2023-12-06 17:57:59 +0200

    Added web app plan and web app

--- main.tf
+++ main.tf
@@ -21,3 +21,25 @@ resource "azurerm_resource_group" "HPA_RG" {
   name     = var.resource_group_name
   location = "West Europe"
 }
+
+resource "azurerm_service_plan" "linux_service_plan" {
+  name                = var.free_web_app_plan
+  resource_group_name = azurerm_resource_group.HPA_RG.name
+  location            = azurerm_resource_group.HPA_RG.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "Health_Prescription"
+  resource_group_name = azurerm_resource_group.HPA_RG.name
+  location            = azurerm_resource_group.HPA_RG.location
+  service_plan_id     = azurerm_service_plan.linux_service_plan.id
+  
+  
+  site_config {
+    application_stack {
+      dotnet_version = 8.0
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "resource_group_name" {
   type        = string
   description = "Resource group name in Azure"
 }
+
+variable "free_web_app_plan" {
+  type        = string
+  description = "Service plan name"
+}
+
+variable "web_app_name" {
+  type = string
+  description = "Web app name"
+}
